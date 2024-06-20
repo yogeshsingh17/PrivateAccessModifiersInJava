6ca186216164ea6ac5a37a1371f3ccf170e9af68
singhyogesh <singhyogesh492@gmail.com>
2024-06-20 20:37:31 +0530

    Commit for private access modifiers in java.

---
+++ PrivateAccessModifierInJava/src/package1/Object_1.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class Object_1 
+{
+	protected void display()
+	{
+		System.out.println("This is Object 1");
+	}
+}
---
+++ PrivateAccessModifierInJava/src/package2/Object_2.java
@@ -0,0 +1,15 @@
+package package2;
+
+import package1.*;
+
+public class Object_2 extends Object_1
+{	
+	public static void main(String[] args) 
+	{
+		Object_2 obj=new Object_2();
+		obj.display();
+		
+
+	}
+
+}
